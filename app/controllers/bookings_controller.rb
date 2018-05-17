1c3b5dd15b59cb28397acaa1834ef0a92474b866
WIlliam Calderon <wec999@gmail.com>
2018-05-17 17:51:59 +0800

    Button aproved and declined working inside a Booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -61,6 +61,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :status)
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,9 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :total_price, presence: true
+
+  def approved
+    self.booking = "Aproved"
+  end
+
 end
