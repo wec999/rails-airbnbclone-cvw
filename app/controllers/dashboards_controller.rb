c6f4aa645d105ec3a0d4b095ed5b70b686b0f254
valerie schraauwers <valerieschraauwers@gmail.com>
2018-05-17 12:32:41 +0800

    Add dashboardcontroller methods, add association to user, has many bookings, change dashboard route and add seed files for flats bookings users

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,4 +1,16 @@
 class DashboardsController < ApplicationController
-  def show
+  before_action :host?
+
+  def host?
+    @host = current_user.flats.any?
   end
+
+  def show
+    @trips = current_user.bookings
+    if @host
+      @my_flats = current_user.flats
+      @my_bookings = Booking.where(flat_id: @my_flats.pluck(:id))
+    end
+  end
+
 end
