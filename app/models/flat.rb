c2b00e438e10c1f1566b17bbb2412bf865e56968
WIlliam Calderon <wec999@gmail.com>
2018-05-18 12:17:03 +0800

    Flat show, new, edit working, without map

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,9 +1,10 @@
 class Flat < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
   belongs_to :user
   has_many :bookings, dependent: :destroy
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :location, presence: true
-  validates :image, presence: true
+  validates :photo, presence: true
 end
