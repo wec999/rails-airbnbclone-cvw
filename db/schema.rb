7d6989377c8be0894b140d042d355134af772069
WIlliam Calderon <wec999@gmail.com>
2018-05-21 13:07:00 +0800

    Seed file incomplete, photo problem

--- db/schema.rb
+++ db/schema.rb
@@ -63,7 +63,7 @@ ActiveRecord::Schema.define(version: 2018_05_21_041611) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name", default: "", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,29 +7,41 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create(email: "john@gmail.com", password: "password")
-User.create(email: "carol@gmail.com", password: "password")
-User.create(email: "bill@gmail.com", password: "password")
-User.create(email: "val@gmail.com", password: "password")
-User.create(email: "bla@bla.com", password: "blabla")
-User.create(email: "la@la.com", password: "lalala")
-User.create(email: "ba@ba.com", password: "bababa")
-User.create(email: "val@val.com", password: "valerie")
+User.create(name: "Valerie Schraauwers", email: "valerie@valerie.com", password: "password")
+User.create(name: "Chelsea Stortz", email: "chelsea@chelsea.com", password: "password")
+User.create(name: "William Calderon", email: "william@william.com", password: "password")
 
-Flat.create(user_id: 1, name: "Mountain View Flat", location: "this address", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "Spend a unforgettable holiday in the enchanting surroundings of the town of Cisternino (reachable from the near airports of Bari and Brindisi).
+Flat.create(user_id: 1, name: "Mountain View Flat", address: "Leticia", photo: "images/hut1.jpg", description: "Spend a unforgettable holiday in the enchanting surroundings of the town of Cisternino (reachable from the near airports of Bari and Brindisi).
 Trullo Edera offers a heaven of peace and tranquillity, set in an elevated position with a stunning view.
 It's the perfect place if you like nature. You can stay under an olive tree reading a good book, you can have a walk in the small country streets or go to the nearest beaches.
-You can even easily visit any of the sights in Apulia such as the caves of Castellana, the trulli of Alberobello, the baroque cities of Lecce and Martina Franca, the excavations of Egnazia, the zoosafari of Fasano, Castel del Monte with Frederick's castle, Grottaglie famous for its ceramics, Taranto, Brindisi and Lecce museums.", price: 50, latitude: 40, longitude: 2)
-Flat.create(user_id: 1, name: "Ocean View Flat", location: "another address", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "overlooking the pacific ocean", price: 50, latitude: 40, longitude: 2)
-Flat.create(user_id: 2, name: "Mountain View Flat 2", location:"address", photo: "photo 3", description: "overlooking ocean with mountain backdrop", price: 50, latitude: 40, longitude: 2)
-Flat.create(user_id: 3, name: "Downtown flat", location: "downtown address", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "beautiful flat - ideal location- central downtown", price: 50, latitude: 40, longitude: 2)
-Flat.create(user_id: 4, name: "Brand New Apartment", location: "new address", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "brand new apartment, ideal location", price: 50, latitude: 40, longitude: 2)
-Flat.create(user_id: 4, name: "Trendy Flat", location: "downtown address", photo: "another photo", description: "another apartment", price: 50, latitude: 40, longitude: 2)
-Flat.create(user_id: 1, name: "Cool flat", location: "this address", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "text", price: 50, latitude: 40, longitude: 2)
-Flat.create(user_id: 4, name: "Awesome flat", location: "this address3", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "text3", price: 50, latitude: 40, longitude: 2)
-Flat.create(user_id: 4, name: "Very cool flat", location: "this address3", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "text3", price: 50, latitude: 40, longitude: 2)
+You can even easily visit any of the sights in Apulia such as the caves of Castellana, the trulli of Alberobello, the baroque cities of Lecce and Martina Franca, the excavations of Egnazia, the zoosafari of Fasano, Castel del Monte with Frederick's castle, Grottaglie famous for its ceramics, Taranto, Brindisi and Lecce museums.",
+price: 50, latitude: -4, longitude: -70)
 
+Flat.create(user_id: 1, name: "Mountain View Flat", address: "Manaus", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "Spend a unforgettable holiday in the enchanting surroundings of the town of Cisternino (reachable from the near airports of Bari and Brindisi).
+Trullo Edera offers a heaven of peace and tranquillity, set in an elevated position with a stunning view.
+It's the perfect place if you like nature. You can stay under an olive tree reading a good book, you can have a walk in the small country streets or go to the nearest beaches.
+You can even easily visit any of the sights in Apulia such as the caves of Castellana, the trulli of Alberobello, the baroque cities of Lecce and Martina Franca, the excavations of Egnazia, the zoosafari of Fasano, Castel del Monte with Frederick's castle, Grottaglie famous for its ceramics, Taranto, Brindisi and Lecce museums.",
+price: 40, latitude: -4, longitude: -70)
 
+Flat.create(user_id: 1, name: "Mountain View Flat", address: "Leticia", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "test", price: 30, latitude: -4, longitude: -70)
+
+Flat.create(user_id: 1, name: "Mountain View Flat", address: "Manaus", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "Spend a unforgettable holiday in the enchanting surroundings of the town of Cisternino (reachable from the near airports of Bari and Brindisi).
+Trullo Edera offers a heaven of peace and tranquillity, set in an elevated position with a stunning view.
+It's the perfect place if you like nature. You can stay under an olive tree reading a good book, you can have a walk in the small country streets or go to the nearest beaches.
+You can even easily visit any of the sights in Apulia such as the caves of Castellana, the trulli of Alberobello, the baroque cities of Lecce and Martina Franca, the excavations of Egnazia, the zoosafari of Fasano, Castel del Monte with Frederick's castle, Grottaglie famous for its ceramics, Taranto, Brindisi and Lecce museums.",
+price: 20, latitude: -4, longitude: -70)
+
+Flat.create(user_id: 2, name: "Mountain View Flat", address: "Leticia", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "Spend a unforgettable holiday in the enchanting surroundings of the town of Cisternino (reachable from the near airports of Bari and Brindisi).
+Trullo Edera offers a heaven of peace and tranquillity, set in an elevated position with a stunning view.
+It's the perfect place if you like nature. You can stay under an olive tree reading a good book, you can have a walk in the small country streets or go to the nearest beaches.
+You can even easily visit any of the sights in Apulia such as the caves of Castellana, the trulli of Alberobello, the baroque cities of Lecce and Martina Franca, the excavations of Egnazia, the zoosafari of Fasano, Castel del Monte with Frederick's castle, Grottaglie famous for its ceramics, Taranto, Brindisi and Lecce museums.",
+price: 10, latitude: -4, longitude: -70)
+
+Flat.create(user_id: 3, name: "Mountain View Flat", address: "Manaus", photo: "image/upload/v1526616977/hyomzbfnzelev0v4tv56.jpg", description: "Spend a unforgettable holiday in the enchanting surroundings of the town of Cisternino (reachable from the near airports of Bari and Brindisi).
+Trullo Edera offers a heaven of peace and tranquillity, set in an elevated position with a stunning view.
+It's the perfect place if you like nature. You can stay under an olive tree reading a good book, you can have a walk in the small country streets or go to the nearest beaches.
+You can even easily visit any of the sights in Apulia such as the caves of Castellana, the trulli of Alberobello, the baroque cities of Lecce and Martina Franca, the excavations of Egnazia, the zoosafari of Fasano, Castel del Monte with Frederick's castle, Grottaglie famous for its ceramics, Taranto, Brindisi and Lecce museums.",
+price: 50, latitude: -4, longitude: -70)
 
 
 Booking.create(start_date: "2018-06-01", end_date: "2018-06-08", total_price: 150, status: "pending", user_id: 1, flat_id: 13)
