1b1063e95921ac9218345d2e5a3376daddb36e51
valerie schraauwers <valerieschraauwers@gmail.com>
2018-05-21 15:06:29 +0800

    added query id to search bar to change border color to same one as navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,13 @@ Rails.application.routes.draw do
   get '/my_flats', to: 'dashboards#my_flats'
   patch '/bookings/:id/approve', to: 'bookings#change_status_approve', as: 'booking_approve'
   patch '/bookings/:id/decline', to: 'bookings#change_status_decline', as: 'booking_decline'
-  get '/home', to: 'pages#home', as: 'home'
 
   resources :flats do
     resources :bookings
   end
 
   devise_for :users, :controllers => {registrations: 'registrations'}
-  root to: 'flats#index'
+  root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
