477efe61b0d11f58f5067c65970d3fdf08fdd248
Chelsea Stortz <chelseastortz@gmail.com>
2018-05-21 16:42:36 +0800

    index page -fixed map and cards

--- db/schema.rb
+++ db/schema.rb
@@ -31,14 +31,15 @@ ActiveRecord::Schema.define(version: 2018_05_21_041611) do
   create_table "flats", force: :cascade do |t|
     t.string "name"
     t.string "address"
+    t.string "image"
     t.text "description"
     t.integer "price"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "photo"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
