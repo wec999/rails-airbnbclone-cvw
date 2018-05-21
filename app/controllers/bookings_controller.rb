319cc3211f9ad043bc8c35fb2692dceb0ec213c4
WIlliam Calderon <wec999@gmail.com>
2018-05-21 18:45:52 +0800

    Reviews added to flat

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,8 +14,8 @@ class BookingsController < ApplicationController
     # CRYPTO PAYMENT
     @address = "1Chain4asCYNnLVbvG6pgCLGBrtzh4Lx4b"
     url = "https://api-r.bitcoinchain.com/v1/address/utxo/#{@address}"
-    response = RestClient.get(url)
-    @data = JSON.parse(response)
+    # response = RestClient.get(url)
+    # @data = JSON.parse(response)
   end
 
   def new
